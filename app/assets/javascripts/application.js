952057a6611fcbe7622ea01d7c95affc8278ef86
newtonkiragu <karanunewton4@gmail.com>
2017-08-29 15:35:27 +0300

    wrote jquerry for parallax and scrollspy

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,3 +13,11 @@
 //= require rails-ujs
 //= require turbolinks
 //= require_tree .
+$(document).ready(function() {
+  $('.carousel').carousel();
+  $('.button-collapse').sideNav();
+  $('.parallax').parallax();
+
+  $('.scrollspy').scrollSpy();
+
+});
